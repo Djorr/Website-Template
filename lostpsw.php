a1c1567534ee2b979ca9dd47120b473f279706a4
Djorr <djorraw@gmail.com>
2020-09-23 12:21:50 +0200

    index, signup en lostpsq.php aangemaakt

---
+++ lostpsw.php
@@ -0,0 +1,5 @@
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  E-mail: <input type="text" name="email">
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
---
+++ signup.php
@@ -0,0 +1,29 @@
+<form action="action_page.php" style="border:1px solid #ccc">
+  <div class="container"><br>
+    <label for="firstname"><b>Voornaam (verplicht)</b></label>
+    <input type="text" placeholder="Vul uw voornaam in" name="email" required><br><br>
+
+    <label for="insertion"><b>Tussenvoegsel (optioneel)</b></label>
+    <input type="text" placeholder="Vul eventuele tussenvoegsel in" name="email"><br><br>
+
+    <label for="lastname"><b>Achternaam (verplicht)</b></label>
+    <input type="text" placeholder="Vul uw achternaam in" name="email" required><br><br>
+
+    <label for="email"><b>Email (verplicht)</b></label>
+    <input type="text" placeholder="Vul uw email adres in" name="email" required><br><br>
+
+    <label for="username"><b>Gebruikersnaam (verplicht)</b></label>
+    <input type="text" placeholder="Vul uw gebruikersnaam in" name="email" required><br><br>
+
+    <label for="psw"><b>Wachtwoord (verplicht)</b></label>
+    <input type="password" placeholder="Vul uw wachtwoord in" name="psw" required><br><br>
+
+    <label for="psw-repeat"><b>Herhaal Wachtwoord (verplicht)</b></label>
+    <input type="password" placeholder="Herhaal uw wachtwoord" name="psw-repeat" required><br><br>
+
+    <div class="clearfix">
+      <button type="submit" class="signupbtn">Sign Up</button>
+      <button type="button" class="cancelbtn">Cancel</button>
+    </div>
+  </div>
+</form>
